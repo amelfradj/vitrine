5cc8cbe773d06858de865b0182806f323625bc7f
amel fradj <amelfradj1@gmail.com>
2025-01-16 18:37:48 +0100

    Fix Webpack configuration for Heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,26 @@
 const Encore = require('@symfony/webpack-encore');
 
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
 Encore
     .setOutputPath('public/build/')
     .setPublicPath('/build')
     .addEntry('app', './assets/app.js')
+    .splitEntryChunks()
     .enableSingleRuntimeChunk()
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
     .enableVersioning(Encore.isProduction())
-    .enableStimulusBridge('./assets/controllers.json')
+    .configureBabel((config) => {
+        config.plugins.push('@babel/plugin-proposal-class-properties');
+    })
     .configureBabelPresetEnv((config) => {
         config.useBuiltIns = 'usage';
-        config.corejs = '3.38';
-    });
+        config.corejs = 3;
+    })
+;
 
-// Exportez la configuration Webpack
-const config = Encore.getWebpackConfig();
-
-// Ajoutez les logs détaillés dans Webpack
-config.stats = 'verbose'; // Affiche des détails sur chaque étape
-
-module.exports = config;
+module.exports = Encore.getWebpackConfig();
